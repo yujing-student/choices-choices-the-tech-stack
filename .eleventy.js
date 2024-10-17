334d7e0a4d0ffb2b26c18268b7eb4dd8309b37ce
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-17 20:53:45 +0200

    error can not get is now correct solved

--- .eleventy.js
+++ .eleventy.js
@@ -5,15 +5,5 @@ export default function(eleventyConfig) {
 
     // hou de css file in de gaten voor aanpassingen
     eleventyConfig.addWatchTarget('css');
-    return {
-        dir: {//naam van het project
-            input: 'src',//dat is de map
-            includes: '_includes',//includes daar staan de tempaltes en partials in
-            output: '_site',//de templates worden in de output gegenereert
-        },
-        templateFormats: ['md', 'njk', 'html'],//dit zijn de template formats
-        markdownTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        dataTemplateEngine: 'njk',
-    };
+
 }
