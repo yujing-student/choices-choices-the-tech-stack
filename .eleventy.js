6a8538e454058fe3330abe4ae639ebed5d8ee3e0
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-17 19:50:30 +0200

    use src for overview on the folders

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,19 @@
-
-
 export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./css/style.css");
 
-    // Watch the `css` directory for changes
+    eleventyConfig.addPassthroughCopy("./css/style.css");
+    // deze regel code kopieert de css naar de outpyt
+
+    // hou de css file in de gaten voor aanpassingen
     eleventyConfig.addWatchTarget('css');
-};
+    return {
+        dir: {//naam van het project
+            input: 'src',//dat is de map
+            includes: '_includes',//includes daar staan de tempaltes en partials in
+            output: '_site',//de templates worden in de output gegenereert
+        },
+        templateFormats: ['md', 'njk', 'html'],//dit zijn de template formats
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        dataTemplateEngine: 'njk',
+    };
+}
