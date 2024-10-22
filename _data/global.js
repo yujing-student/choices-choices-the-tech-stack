35372bdb1142e94b18cc6a3dbb868d2d02ef9270
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-22 21:18:03 +0200

    feature:i added data from the database

--- _data/global.js
+++ _data/global.js
@@ -1,9 +1,11 @@
-import agencies from './agencies.js';
+import agencies from './directus.js';
 import { readItems } from '@directus/sdk';
 
 export default async () => {
 
-    return await agencies.request(readItems('dda_agencies')).catch(
+    return await agencies.request(readItems('dda_agencies'))
+
+        .catch(
         err => console.log(err)
     )
 
