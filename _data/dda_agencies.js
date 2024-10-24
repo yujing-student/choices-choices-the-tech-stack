054fa410cbda6be76bba967b8ee0ae9dbbcc9729
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-24 13:23:28 +0200

    feature: i added a component intro on the 2 pages and i added comments about the code in _data files

--- _data/dda_agencies.js
+++ _data/dda_agencies.js
@@ -1,15 +1,3 @@
-// import agencies from './directus.js';
-// import { readItems } from '@directus/sdk';
-//
-// export default async () => {
-//
-//     return await agencies.request(readItems('dda_agencies')).catch(err => console.log(err)
-//     )
-//
-//
-// }
-
-
 // importeer de directus.js
 import agencies from './directus.js';
 // lees de items uit de database
--- _data/directus.js
+++ _data/directus.js
@@ -1,8 +1,11 @@
 import { createDirectus, rest } from '@directus/sdk';
 
+
+// defineer een variable met de directus database en maak een api call naar de database
 const agencies = createDirectus('https://fdnd-agency.directus.app/')
     .with(rest());
 
+// exporteer dit variable zodat het gebruikt kan worden op de andere pagina's
 export default agencies;
 
 
