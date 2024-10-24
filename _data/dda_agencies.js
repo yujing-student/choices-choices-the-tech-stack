ada65a6d76e10a1ffcebbe3cb64a798dc268ce28
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-10-24 11:45:31 +0200

    i added comments about the code in the dda_agencies.js

--- _data/dda_agencies.js
+++ _data/dda_agencies.js
@@ -10,17 +10,19 @@
 // }
 
 
-
+// importeer de directus.js
 import agencies from './directus.js';
+// lees de items uit de database
 import { readItems } from '@directus/sdk';
 
 export default async () => {
     try {
+        // verzoek om de items uit te lezen
         const data = await agencies.request(readItems('dda_agencies'));
-        console.log(data); // Log the fetched data to the console
-        return data; // Return the data for further use
+        console.log(data); // laat de opgehaalde data zien
+        return data; // geef de data mee
     } catch (err) {
-        console.error('Error fetching data:', err);
-        return null; // Or throw the error if you prefer
+        // een catch als de data niet word opgehaald
+        console.error('Error de data word niet opgehaald', err);
     }
 }
