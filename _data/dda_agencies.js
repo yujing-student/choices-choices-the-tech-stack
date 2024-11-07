2b255b7f3046926ce09f327082cfff1d51529b68
yujing-student <100352887+yujing-student@users.noreply.github.com>
2024-11-07 18:48:33 +0100

    fix:remove console log and add comment how to start the server

--- _data/dda_agencies.js
+++ _data/dda_agencies.js
@@ -7,7 +7,7 @@ export default async () => {
     try {
         // verzoek om de items uit te lezen
         const data = await agencies.request(readItems('dda_agencies'));
-        console.log(data); // laat de opgehaalde data zien
+        // console.log(data); // laat de opgehaalde data zien
         return data; // geef de data mee
     } catch (err) {
         // een catch als de data niet word opgehaald
